39e7b3b189364f097e23e1036e96b412413121bb
AnnaZhizhiashvili <Ana1998@protonmail.com>
2021-02-13 23:26:25 -1200

    ...

--- myapp.js
+++ myapp.js
@@ -32,6 +32,12 @@ sections.forEach(section => sectionObserver.observe(section)
 //skill-box width increasing
 
 const widths = [...document.querySelectorAll(".starting-width")];
+const counters = document.querySelectorAll(".percentage");
+
+
+
+
+
 
 let widthOptions = {
 	threshold: 1
@@ -43,15 +49,43 @@ const skillsObserver = new IntersectionObserver(function(entries) {
 					widths[1].classList.add("width2");
 					widths[2].classList.add("width3");
 					widths[3].classList.add("width4");
-					console.log(widths[0]);
+					counters.forEach(counter => {
+					const updateCount = () => {
+					const target = +counter.getAttribute("data-target");
+					const percentage = +counter.innerText;
+
+						if (percentage < target) {
+							counter.innerText = percentage + 1;
+							setTimeout(updateCount, 45)
+
+					}	else {
+						counter.innerText = target;
+					}
+
+					}
+						updateCount();
+					})
 				}
 			})
-			
-}, widthOptions);
+	}, widthOptions);
 
 widths.forEach(w => skillsObserver.observe(w)
  );
 
+//resume section, skewing boxes 
+const skewedObjects = document.querySelectorAll("#swing");
+skewedOptions = {
+	threshold: .6
+}
+const skewObserver = new IntersectionObserver(function(entries){
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("swing");
+		}
+	}
+)}, skewedOptions);
+
+skewedObjects.forEach(object => skewObserver.observe(object))
 
 
  
